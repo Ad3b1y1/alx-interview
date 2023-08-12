8b86c159e00b1900ab2339e43afa2e565f7fa5ef
Ad3b1y1 <opeoluwaadebiyi@outlook.com>
2023-08-12 23:04:04 +0100

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,26 @@
 #!/usr/bin/node
+
 const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
+  request(`${API_URL}/films/${process.argv[2]}/`, (error, _, body) => {
+    if (error) {
+      console.log(error);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
+    const charactersURLs = JSON.parse(body).characters;
+    const charactersNames = charactersURLs.map(
       url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
+        request(url, (promiseError, __, charactersReqBody) => {
+          if (promiseError) {
+            reject(promiseError);
           }
           resolve(JSON.parse(charactersReqBody).name);
         });
       }));
 
-    Promise.all(charactersName)
+    Promise.all(charactersNames)
       .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+      .catch(allErrors => console.log(allErrors));
   });
 }
