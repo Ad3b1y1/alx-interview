ded886225e537099fe8257d333b8a4cf11b96abb
Ad3b1y1 <opeoluwaadebiyi@outlook.com>
2023-08-25 02:10:35 +0100

    interview

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,57 +1,25 @@
 #!/usr/bin/python3
-"""
-Task: Inner Transformation of Change
-Given a collection of coins with different values,
-calculate the minimum number of coins required to
-reach a specified total amount.
-
-Parameters:
-- coins: A list of coin values
-- total: The target total amount
-
-Returns:
-- The minimum number of coins needed to reach the total
-  - Returns 0 if the total is 0 or negative
-  - Returns -1 if the total cannot be achieved with available coins
-
-Notes:
-- The value of each coin is always a positive integer
-- It is assumed that there is an unlimited supply of each denomination
-
+"""Change making module.
 """
 
-def calculateMinCoins(coins, total):
-    """
-    Given a collection of coins with different values,
-    calculate the minimum number of coins required to
-    reach a specified total amount.
-    
-    Parameters:
-    - coins: A list of coin values
-    - total: The target total amount
-    
-    Returns:
-    - The minimum number of coins needed to reach the total
-      - Returns 0 if the total is 0 or negative
-      - Returns -1 if the total cannot be achieved with available coins
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
     if total <= 0:
         return 0
-    
-    max_val = total + 1
-    coin_counts = {0: 0}
-    
-    for i in range(1, total + 1):
-        coin_counts[i] = max_val
-        
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
-            
-            coin_counts[i] = min(coin_counts[current] + 1, coin_counts[i])
-    
-    if coin_counts[total] == total + 1:
-        return -1
-    
-    return coin_counts[total]
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
